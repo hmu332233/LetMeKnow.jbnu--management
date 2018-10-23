b0aa0f34c52a0eaabbd24f13ef001a2ed2b0ba29
minung.han <hmu332233@gmail.com>
2018-10-24 00:53:06 +0900

    [Modify] findByDate에 날짜 포멧 고정

--- server/models/UserWords.js
+++ server/models/UserWords.js
@@ -16,12 +16,12 @@ const UserWordsModule = {
   },
   /*
   *  특정 날짜 구역내를 검색한다
-  *  @param {Date} start 시작날짜
-  *  @param {Date} end 끝 날짜
+  *  @param {Date} start 시작날짜 ex) 2018-10-24
+  *  @param {Date} end 끝 날짜 ex) 2018-10-24
   */
   findByDate: async ({ start , end }) => {
-    start = start || moment().startOf('day');
-    end = end || moment(start).endOf('day');
+    start = start && moment(start).startOf('day') || moment().startOf('day');
+    end = end && moment(start).endOf('day') || moment(start).endOf('day');
 
     try {
       const userWords = await UserWords.find({
