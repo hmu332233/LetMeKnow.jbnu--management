37244c84bd4b9d400bbbaf2ba831ed598c635068
minung.han <hmu332233@gmail.com>
2018-11-01 01:23:49 +0900

    [Modify] userWordRef 외부로 빼기

--- server/models/UserWordsWithFirebase.js
+++ server/models/UserWordsWithFirebase.js
@@ -1,9 +1,11 @@
 const { firebase, db } = require('../db/firebaseInit');
 const moment = require('moment');
 
+const userWordRef = db.collection('user_words');
+
 const UserWordsModule = {
   set: ({ id, content }) => {
-    const newUserWord = db.collection('user_words').doc();
+    const newUserWord = userWordRef.doc();
     newUserWord.set({ 
       id,
       content,
@@ -20,7 +22,6 @@ const UserWordsModule = {
     start = start && moment(start).startOf('day') || moment().startOf('day');
     end = end && moment(start).endOf('day') || moment(start).endOf('day');
  
-    const userWordRef = db.collection('user_words');
     const userWordSnapshot = await userWordRef.where('timestamp', '>=', start.unix()).where('timestamp', '<', end.unix()).orderBy("timestamp", "desc").get();
 
     if (userWordSnapshot.empty) {
