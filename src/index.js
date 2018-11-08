6ec9e8d31db89378443b362555df449b5fda6ea4
minung.han <hmu332233@gmail.com>
2018-11-08 22:12:12 +0900

    [Remove] bootstrap, reactstrap [Add] tabler-react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainApp from 'apps/MainApp';
+import 'tabler-react/dist/Tabler.css';
 
 ReactDOM.render(<MainApp />, document.getElementById('app'));
