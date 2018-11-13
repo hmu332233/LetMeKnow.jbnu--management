879c738610890c940ac6cfe7b4fe803dfa4fcd0a
minung.han <hmu332233@gmail.com>
2018-11-13 23:49:24 +0900

    [Add] charts default label

--- src/pages/Charts/Charts.jsx
+++ src/pages/Charts/Charts.jsx
@@ -52,7 +52,7 @@ class Charts extends React.Component {
     const chart = new Chartist.Line(
       '.ct-chart',
       {
-        labels,
+        labels: ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'],
         series
       },
       {
