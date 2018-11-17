2bb885d94407dcc2e66278a4f5b7bc7aa7a17efc
minung.han <hmu332233@gmail.com>
2018-11-18 03:20:25 +0900

    [Add] datePicker

--- src/pages/Charts/Charts.jsx
+++ src/pages/Charts/Charts.jsx
@@ -10,23 +10,42 @@ import BasicLayout from 'components/BasicLayout';
 import HourlyUsageChartCard from 'components/HourlyUsageChartCard';
 import StatsCard from 'components/StatsCard';
 
+import moment from 'moment';
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
 class Charts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      userWords: []
+      userWords: [],
+      startDate: moment(),
+      endDate: moment(),
     };
 
     this.fetchUserWords = this.fetchUserWords.bind(this);
+    this.handleDatesChange = this.handleDatesChange.bind(this);
   }
 
   componentDidMount() {
-    this.fetchUserWords();
+    this.fetchUserWords({
+      startDate: this.state.startDate
+    });
   }
 
-  fetchUserWords() {
-    axios.get('/api/v1/user_words/date').then(res => {
+  handleDatesChange({ startDate, endDate }) {
+    this.setState({ startDate, endDate, isLoading: true });
+    this.fetchUserWords({ startDate, endDate });
+  }
+
+  fetchUserWords({ startDate, endDate }) {
+    const params = {
+      start: startDate && startDate.format('YYYYMMDD'),
+      end: endDate && endDate.format('YYYYMMDD')
+    }
+    axios.get('/api/v1/user_words/date', { params }).then(res => {
       if (res.data.success) {
         this.setState({
           isLoading: false,
@@ -41,6 +60,18 @@ class Charts extends React.Component {
     return (
       <BasicLayout>
         <div className={styles.Charts}>
+          <DateRangePicker
+            displayFormat="YYYY-MM-DD"
+            startDate={this.state.startDate}
+            startDateId="start_date_id"
+            endDate={this.state.endDate}
+            endDateId="end_date_id"
+            onDatesChange={this.handleDatesChange}
+            focusedInput={this.state.focusedInput}
+            onFocusChange={focusedInput => this.setState({ focusedInput })}
+            minimumNights={0}
+            hideKeyboardShortcutsPanel
+          />
           <Row>
             <Col xs="6" sm="4" lg="2">
               <StatsCard label="calls" total={this.state.userWords.length} />
