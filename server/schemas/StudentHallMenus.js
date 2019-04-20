1c5bcf492c6bbb2bb0364a8c5f61a7baba1caa72
minung.han <hmu332233@gmail.com>
2019-04-20 21:35:44 +0900

    [Add] 진수당, 학생회관 메뉴 가져오는 api 추가

---
+++ server/schemas/StudentHallMenus.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+/*
+*  StudentHallMenus
+*  lunch {Array} 한 주 동안의 점심 정보
+*  dinner {Array} 한 주 동안의 저녁 정보
+*/
+// schema
+const schema = mongoose.Schema({
+  lunch: [
+		{
+			place: String,
+			week: String,
+			time: String,
+			category: String,
+			menus: String
+		}
+	],
+	dinner: [
+		{
+			place: String,
+			week: String,
+			time: String,
+			category: String,
+			menus: String
+		}
+	]
+});
+
+// model & export
+const StudentHallMenus = mongoose.model('student_hall_menus', schema);
+module.exports = StudentHallMenus;
