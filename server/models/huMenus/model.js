0cf5316b72cb414275e4638a9c25feec289dcffb
minung.han <hmu332233@gmail.com>
2019-05-17 00:32:54 +0900

    [Add] huMenus model

---
+++ server/models/huMenus/model.js
@@ -0,0 +1,23 @@
+const { db } = require('./info');
+
+const { utils } = require('../../utils');
+
+const HuMenus = {
+	findOne: async function (query, { select, sort } = {}) {
+		try {
+			const menu = await db.he_menus.findOne(query).select(select).sort(sort).lean();
+			return menu;
+		} catch (err) {
+			throw utils.mongoFormat.error(err);
+		}
+	},
+	getLatestMenu: async function () {
+		const query = {};
+		const options = {
+			sort: { _id: -1 }
+		};
+		return await this.findOne(query, options);
+	}
+}
+
+module.exports = HuMenus;
