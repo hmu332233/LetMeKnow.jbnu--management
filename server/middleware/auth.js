c0a51c9321b1723fe1d8b61fe231998d5693c2a3
minung.han <hmu332233@gmail.com>
2019-06-09 03:18:46 +0900

    [Modify] 로그인한 유저만 화면에 들어올 수 있도록 수정

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -13,3 +13,14 @@ exports.check = () => async (req, res, next) => {
   req.userData = user;
   next();
 }
+
+exports.checkView = () => async (req, res, next) => {
+  const token = req.cookies['x-access-token'] || req.query.token;
+  const { isVerified, _id } = service.auth.verifyToken({ token });
+  if (!isVerified) {
+    return res.redirect('/login');
+  }
+  const user = await Users.findBy_id({ _id });
+  req.userData = user;
+  next();
+}
