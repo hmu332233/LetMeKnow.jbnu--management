95b25e90285be3f56ac86c13b0287e4b9bced067
minung.han <hmu332233@gmail.com>
2019-07-01 00:39:51 +0900

    build: modify travis.yml

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 
-git config user.email "travis@travis-ci.org";
-git config user.name "Travis CI";
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-git add --all build;
-git commit -m "Travis build";
+commit_country_json_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add --all build
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
 
-git remote add origin-pages https://${token}@github.com/hmu332233/LetMeKnow.jbnu-management.git > /dev/null 2>&1;
-git push --quiet --set-upstream origin-pages develop;
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://${token}@github.com/hmu332233/LetMeKnow.jbnu-management.git > /dev/null 2>&1;
+  git push origin master --quiet
+}
+
+setup_git
+commit_country_json_files
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "A new commit with changed country JSON files exists. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes in country JSON files. Nothing to do"
+fi
