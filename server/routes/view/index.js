1600263dbc96637229303294ecd8a47ff22dcbd6
minung.han <hmu332233@gmail.com>
2019-10-17 00:57:20 +0900

    fix: dotenv 기반으로 변경 안된 일부 코드 수정

--- server/routes/view/index.js
+++ server/routes/view/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const middleware = require('../../middleware');
 
 router.get(['/login'], (req, res) => {
-  if (NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     res.sendFile(path.join(__dirname + './../../../dist/index.html'));
   } else {
     res.sendFile(path.join(__dirname + './../../../build/index.html'));
@@ -13,7 +13,7 @@ router.get(['/login'], (req, res) => {
 });
 
 router.get(['/', '/usage', '/summary', '/chat', '/settings'], middleware.auth.checkView(), (req, res) => {
-  if (NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development') {
     res.sendFile(path.join(__dirname + './../../../dist/index.html'));
   } else {
     res.sendFile(path.join(__dirname + './../../../build/index.html'));
