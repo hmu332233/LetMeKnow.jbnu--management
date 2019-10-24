a307a7344e8adea98b7691591504e6d620b18359
minung.han <hmu332233@gmail.com>
2019-10-25 03:08:06 +0900

    test: routes 테스트 코드 추가

---
+++ server/test/routes/api/menus.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const app = require('../../../app');
+
+describe('menus', () => {
+  test('medi - status code 200', done => {
+    request(app)
+      .get('/api/v1/menus/medi')
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+  test('jinsu - status code 200', done => {
+    request(app)
+      .get('/api/v1/menus//jinsu')
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+  test('studentHall - status code 200', done => {
+    return request(app)
+      .get('/api/v1/menus//studentHall')
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+  test('jungdam - status code 200', done => {
+    return request(app)
+      .get('/api/v1/menus//jungdam')
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+  test('hu - status code 200', done => {
+    return request(app)
+      .get('/api/v1/menus//hu')
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+});
