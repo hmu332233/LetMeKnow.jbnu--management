2a9cec30229d22143ba62bfc1e58edbe70c0147b
minung.han <hmu332233@gmail.com>
2019-11-12 02:27:26 +0900

    feat: addon.addQuickReply 함수 및 테스트 코드 추가

--- server/controllers/addon/index.js
+++ server/controllers/addon/index.js
@@ -2,6 +2,21 @@ const { format } = require('./../../utils');
 
 const QuickReplies = require('./../../models/quickReplies');
 
+exports.addQuickReply = async (req, res, next) => {
+  const { label, messageText, start, end } = req.body;
+  try {
+    const result = await QuickReplies.add({
+      label,
+      messageText,
+      start: start && new Date(start),
+      end: end && new Date(end),
+    });
+    res.json(format.response.successTrue(result));
+  } catch (err) {
+    next(err);
+  }
+};
+
 exports.getQuickReplies = async (req, res, next) => {
   try {
     const result = await QuickReplies.findByDate({ date: new Date() });
--- server/controllers/addon/index.test.js
+++ server/controllers/addon/index.test.js
@@ -34,5 +34,41 @@ describe('controller - addon', () => {
         data: [mockQuizReply],
       });
     });
+
+    test('add - label이 없는 경우 next 처리', async () => {
+      const req = {
+        body: { messageText: 'test' },
+      };
+      const res = mockResponse();
+      const next = jest.fn();
+      await controller.addon.addQuickReply(req, res, next);
+      expect(next).toHaveBeenCalledTimes(1);
+    });
+
+    test('add - messageText가 없는 경우 next 처리', async () => {
+      const req = {
+        body: { label: 'test ' },
+      };
+      const res = mockResponse();
+      const next = jest.fn();
+      await controller.addon.addQuickReply(req, res, next);
+      expect(next).toHaveBeenCalledTimes(1);
+    });
+
+    test('add - 모든 데이터가 다 있을 경우 정상 실행', async () => {
+      QuickReplies.add = jest.fn().mockResolvedValue(mockQuizReply);
+      const req = {
+        body: mockQuizReply,
+      };
+      const res = mockResponse();
+      const next = v => v;
+      await controller.addon.addQuickReply(req, res, next);
+      expect(res.json).toHaveBeenCalledWith({
+        success: true,
+        message: null,
+        errors: null,
+        data: mockQuizReply,
+      });
+    });
   });
 });
