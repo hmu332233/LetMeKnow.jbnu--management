35c3aea3912de91b32d7ba2d87cfdb196a44c954
minung.han <hmu332233@gmail.com>
2019-11-12 02:54:37 +0900

    feat: addQuickReply api 및 테스트 코드 추가

--- server/routes/api/addon.js
+++ server/routes/api/addon.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require('../../controllers');
 
 router.get('/quick-replies', controller.addon.getQuickReplies);
+router.post('/quick-replies', controller.addon.addQuickReply);
 
 module.exports = router;
--- server/test/routes/api/addon.test.js
+++ server/test/routes/api/addon.test.js
@@ -5,9 +5,45 @@ const { setupDB } = require('../../setupDb');
 setupDB();
 
 describe('addon', () => {
-  test('quickReplies - status code 200', done => {
+  test('get quickReplies - status code 200', done => {
     request(app)
       .get('/api/v1/addon/quick-replies')
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+  });
+
+  test('post quickReplies - status code 400 - without label', done => {
+    request(app)
+      .post('/api/v1/addon/quick-replies')
+      .send({ messageText: 'test' })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .then(res => {
+        expect(res.statusCode).toBe(400);
+        done();
+      });
+  });
+
+  test('post quickReplies - status code 400 - without messageText', done => {
+    request(app)
+      .post('/api/v1/addon/quick-replies')
+      .send({ label: 'test' })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .then(res => {
+        expect(res.statusCode).toBe(400);
+        done();
+      });
+  });
+
+  test('post quickReplies - status code 200', done => {
+    request(app)
+      .post('/api/v1/addon/quick-replies')
+      .send({ label: 'test', messageText: 'test' })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
       .then(res => {
         expect(res.statusCode).toBe(200);
         done();
