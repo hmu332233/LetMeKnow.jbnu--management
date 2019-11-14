b77189319030117f8ced7ba60d0c2e075e471dab
root <smctkdqja@gmail.com>
2019-11-14 15:15:18 +0000

    식단api에서 내용이 비어있을 경우에 대한 처리 수정본

--- server/services/menus.js
+++ server/services/menus.js
@@ -6,25 +6,84 @@ const HuMenus = require('../models/huMenus');
 
 exports.getMediMenus = async () => {
   const menu = await MediMenus.getLatestMenu();
+	if(menu == null){
+		return menu;
+	}
+	else{
+		if(menu.lunch[0].menus == ""){
+			menu.lunch[0].menus = "등록된 식단이 없습니다.";
+		}
+
+		if(menu.dinner[0].menus == ""){
+			menu.dinner[0].menus = "등록된 식단이 없습니다.";
+		}
+	}
+    
   return menu;
 };
 
 exports.getJinsuMenus = async () => {
   const menu = await JinsuMenus.getLatestMenu();
+	if(menu == null){
+		return menu;
+	}
+	else{
+		if(menu.lunch[0].menus == ""){
+			menu.lunch[0].menus = "등록된 식단이 없습니다.";
+		}
+
+		if(menu.dinner[0].menus == ""){
+			menu.dinner[0].menus = "등록된 식단이 없습니다.";
+		}
+	}
   return menu;
 };
 
 exports.getStudentHallMenus = async () => {
   const menu = await StudentHallMenus.getLatestMenu();
+	if(menu == null){
+		return menu;
+	}
+	else{
+		if(menu.lunch[0].menus == ""){
+			menu.lunch[0].menus = "등록된 식단이 없습니다.";
+		}
+
+		if(menu.dinner[0].menus == ""){
+			menu.dinner[0].menus = "등록된 식단이 없습니다.";
+		}
+	}
   return menu;
 };
 
 exports.getJungdamMenus = async () => {
   const menu = await JungdamMenus.getLatestMenu();
+	if(menu == null){
+		return menu;
+	}
+	else{
+		if(menu.lunch[0].menus == ""){
+			menu.lunch[0].menus = "등록된 식단이 없습니다.";
+		}
+	}
   return menu;
 };
 
 exports.getHuMenus = async () => {
   const menu = await HuMenus.getLatestMenu();
+	if(menu == null){
+		return menu;
+	}
+	else{
+		if(menu.breakfast[0].menus == ""){
+			menu.breakfast[0].menus = "등록된 식단이 없습니다.";
+		}
+		if(menu.lunch[0].menus == ""){
+			menu.lunch[0].menus = "등록된 식단이 없습니다.";
+		}
+		if(menu.dinner[0].menus == ""){
+			menu.dinner[0].menus = "등록된 식단이 없습니다.";
+		}
+	}
   return menu;
 };
