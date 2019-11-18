19cfdb0cee2f405cf65261896441943a3bf7ecc7
root <smctkdqja@gmail.com>
2019-11-18 12:32:55 +0000

    로직을 함수로 묶어 문구가 변경되거나 메뉴에 대한 처리가 추가로 필요할때 코드만 수정하면 되도록 수정

--- server/services/menus.js
+++ server/services/menus.js
@@ -4,122 +4,69 @@ const StudentHallMenus = require('../models/studentHallMenus');
 const JungdamMenus = require('../models/jungdamMenus');
 const HuMenus = require('../models/huMenus');
 
-exports.getMediMenus = async () => {
-  const menu = await MediMenus.getLatestMenu();	
-	
-	if(menu === null){
-	  return menu;
-	}
-	else{
-      var obj_lunch_len = Object.keys(menu.lunch).length;
-	  var obj_dinner_len = Object.keys(menu.dinner).length;
+const RedesignMenu = meals => meals.map(meal => ({ ...meal, menus: meal.menus || '등록된 식단이 없습니다.' }));
 
-      for(var i=0; i<obj_lunch_len; i++){
-        if(menu.lunch[i].menus === ''){
-          menu.lunch[i].menus = '등록된 식단이 없습니다.';
-        }
-      }
-	  for(var i=0; i<obj_dinner_len; i++){
-        if(menu.dinner[i].menus === ''){
-          menu.dinner[i].menus = '등록된 식단이 없습니다.';
-	    }
-	  }
-	}    
+exports.getMediMenus = async () => {
+  const menu = await MediMenus.getLatestMenu();
+
+  if (!menu) {
+    return null;
+  }
+
+  menu.lunch = RedesignMenu(menu.lunch);
+  menu.dinner = RedesignMenu(menu.dinner);
+
   return menu;
 };
 
 exports.getJinsuMenus = async () => {
-  const menu = await JinsuMenus.getLatestMenu();	
-	
-	if(menu === null){
-	  return menu;
-	}
-	else{
-      var obj_lunch_len = Object.keys(menu.lunch).length;
-	  var obj_dinner_len = Object.keys(menu.dinner).length;	
+  const menu = await JinsuMenus.getLatestMenu();
 
-      for(var i=0; i<obj_lunch_len; i++){
-        if(menu.lunch[i].menus === ''){
-          menu.lunch[i].menus = '등록된 식단이 없습니다.';
-        }
-      }
-	  for(var i=0; i<obj_dinner_len; i++){
-        if(menu.dinner[i].menus === ''){
-          menu.dinner[i].menus = '등록된 식단이 없습니다.';
-	    }
-	  }
-	}    
+  if (!menu) {
+    return null;
+  }
+
+  menu.lunch = RedesignMenu(menu.lunch);
+  menu.dinner = RedesignMenu(menu.dinner);
+
   return menu;
 };
 
 exports.getStudentHallMenus = async () => {
-  const menu = await StudentHallMenus.getLatestMenu();	
-	
-	if(menu === null){
-	  return menu;
-	}
-	else{
-      var obj_lunch_len = Object.keys(menu.lunch).length;
-	  var obj_dinner_len = Object.keys(menu.dinner).length;
+  const menu = await StudentHallMenus.getLatestMenu();
 
-      for(var i=0; i<obj_lunch_len; i++){
-        if(menu.lunch[i].menus === ''){
-          menu.lunch[i].menus = '등록된 식단이 없습니다.';
-        }
-      }
-	  for(var i=0; i<obj_dinner_len; i++){
-        if(menu.dinner[i].menus === ''){
-          menu.dinner[i].menus = '등록된 식단이 없습니다.';
-	    }
-	  }
-	}    
+  if (!menu) {
+    return null;
+  }
+
+  menu.lunch = RedesignMenu(menu.lunch);
+  menu.dinner = RedesignMenu(menu.dinner);
+
   return menu;
 };
 
 exports.getJungdamMenus = async () => {
   const menu = await JungdamMenus.getLatestMenu();
-	
-	if(menu === null){
-	  return menu;
-	}
-	else{
-      var obj_lunch_len = Object.keys(menu.lunch).length;
 
-      for(var i=0; i<obj_lunch_len; i++){
-        if(menu.lunch[i].menus === ''){
-          menu.lunch[i].menus = '등록된 식단이 없습니다.';
-        }
-      }
-	}    
+  if (!menu) {
+    return null;
+  }
+
+  menu.lunch = RedesignMenu(menu.lunch);
+
   return menu;
 };
 
 exports.getHuMenus = async () => {
-  const menu = await HuMenus.getLatestMenu();	
-	
-	if(menu === null){
-	  return menu;
-	}
-	else{
-      var obj_breakfast_len = Object.keys(menu.breakfast).length;
-      var obj_lunch_len = Object.keys(menu.lunch).length;
-	  var obj_dinner_len = Object.keys(menu.dinner).length;
+  const menu = await HuMenus.getLatestMenu();
 
-	  for(var i=0; i<obj_breakfast_len; i++){
-        if(menu.breakfast[i].menus === ''){
-          menu.breakfast[i].menus = '등록된 식단이 없습니다.';
-        }
-      }	
-      for(var i=0; i<obj_lunch_len; i++){
-        if(menu.lunch[i].menus === ''){
-          menu.lunch[i].menus = '등록된 식단이 없습니다.';
-        }
-      }
-	  for(var i=0; i<obj_dinner_len; i++){
-        if(menu.dinner[i].menus === ''){
-          menu.dinner[i].menus = '등록된 식단이 없습니다.';
-	    }
-	  }
-	}    
+  if (!menu) {
+    return null;
+  }
+
+  menu.breakfast = RedesignMenu(menu.breakfast);
+  menu.lunch = RedesignMenu(menu.lunch);
+  menu.dinner = RedesignMenu(menu.dinner);
+
   return menu;
 };
